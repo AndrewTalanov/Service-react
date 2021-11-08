70a1d06a3ee0ce156f9b71b92335076d8deb015d
Andrew Talanov <andrewtalanov@mail.ru>
2021-11-08 20:35:29 +0500

    готово боковое меню

--- src/sidebar/sidebar-list-item/sidebar-list-item.js
+++ src/sidebar/sidebar-list-item/sidebar-list-item.js
@@ -2,9 +2,7 @@ import './sidebar-list-item.css';
 
 const SidebarListItem = () => {
   return (
-    <div className="sidebar-list-item">
-      <img src='./images/one.png' alt="" />
-    </div>
+    <div></div>
   )
 }
 
