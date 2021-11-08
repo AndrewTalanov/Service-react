53932087a5e87a1618cf26e1ac5ea89e29500cca
AndrewBavyka <abavyka@list.ru>
2021-11-08 20:48:20 +0500

    сделали шапку

--- src/header/header.js
+++ src/header/header.js
@@ -2,7 +2,22 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div className="header"></div>
+    <div className="header">
+          <div className="change__city">
+            <div className="change__city-container">
+              <p>Москва</p>
+              <img src="./images/change-city-icon.svg" alt="img-error" />
+            </div>      
+         </div>
+        <div className="header__wrapper">
+            <div className="title">
+              <h1>Доступные рестораны</h1>
+            </div>
+          <div className="search__container">
+            <img src="./images/icon-search.svg" alt="img-error" />
+          </div>
+        </div>    
+    </div>
   )
 }
 
