7b8e3493a27afcb1ad7a6a951cb5a6266994a4ba
Andrew Talanov <andrewtalanov@mail.ru>
2021-11-13 17:35:34 +0500

    адаптив

--- src/sidebar/sidebar-list-button/sidebar-list-button.js
+++ src/sidebar/sidebar-list-button/sidebar-list-button.js
@@ -2,12 +2,10 @@ import './sidebar-list-button.css'
 
 const SidebarListButton = () => {
   return (
-    <div className="sidebar-list__active">
-        <div className="block-one"></div>
-        <div className="block-two"></div>
-        <div className="border-one"></div>
-        <div className="border-two"></div>
-        <div className="sidebar-list__btn"></div>
+    <div className="sidebar-list-wrapper">
+      <div className="after"></div>
+      <div className="sidebar-list__btn"></div>
+      <div className="before"></div>
     </div>
   )
 }
