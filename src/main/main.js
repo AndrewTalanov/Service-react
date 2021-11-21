8d3affe889e907587f822d92a602e515ab8f431f
AndrewBavyka <abavyka@list.ru>
2021-11-21 15:40:43 +0500

    to fix bugs

--- src/main/main.js
+++ src/main/main.js
@@ -1,76 +1,23 @@
+import MainCardList from './main-card-list/main-card-list';
 import './main.css';
 
 const Main = () => {
+
+  const data = [
+    {id: 1, src: "./images/restaurant_1.png", name: "Полли", adress: "Молодогвардейцев, 84", time: "12:00 - 22:00"},
+    {id: 2, src: "./images/restaurant_1.png", name: "Vолли", adress: "Молодогвардейцев, 84", time: "12:00 - 22:00"},
+    {id: 3, src: "./images/restaurant_1.png", name: "Молли", adress: "Молодогвардейцев, 84", time: "12:00 - 22:00"},
+    {id: 4, src: "./images/restaurant_1.png", name: "Колли", adress: "Молодогвардейцев, 84", time: "12:00 - 22:00"},
+    {id: 5, src: "./images/restaurant_1.png", name: "Толли", adress: "Молодогвардейцев, 84", time: "12:00 - 22:00"},
+    {id: 6, src: "./images/restaurant_1.png", name: "Толли", adress: "Молодогвардейцев, 84", time: "12:00 - 22:00"},
+    {id: 7, src: "./images/restaurant_1.png", name: "Толли", adress: "Молодогвардейцев, 84", time: "12:00 - 22:00"},
+    {id: 8, src: "./images/restaurant_1.png", name: "Толли", adress: "Молодогвардейцев, 84", time: "12:00 - 22:00"},
+    {id: 9, src: "./images/restaurant_1.png", name: "Толли", adress: "Молодогвардейцев, 84", time: "12:00 - 22:00"},
+  ]
+
   return (
     <div className="main">
-        <div className="main__content">
-          <div className="restaurant-container">
-              <div className="restaurant-container__card">
-                <div className="restaurant_container__card-naming">
-                    <img src="./images/restaurant_1.png" alt="img-error" />
-                          <p>Полли</p>
-                        <div className="restaurant_container__card-discription">
-                          <div className="restaurant_container_card__discription-contact">
-                             <img src="./images/restaurant_stars.png" alt="img-error" />
-                             <p>Молодогвардейцев, 84</p>
-                          </div>
-                          <div className="restaurant_container_card__discription-price">
-                              <img src="./images/price.png" alt="img-error" />
-                              <p>12:00 - 22:00</p>
-                          </div>                                            
-                      </div>
-                  </div>
-              </div>
-              <div className="restaurant-container__card">
-                  <div className="restaurant_container__card-naming">
-                   <img src="./images/restaurant_2.png" alt="img-error" />
-                          <p>Ривердейл</p>
-                        <div className="restaurant_container__card-discription">
-                          <div className="restaurant_container_card__discription-contact">
-                             <img src="./images/restaurant_stars.png" alt="img-error" />
-                             <p>Российская, 84</p>
-                          </div>
-                          <div className="restaurant_container_card__discription-price">
-                              <img src="./images/price.png" alt="img-error" />
-                              <p>12:00 - 22:00</p>
-                          </div>                                            
-                      </div>
-                  </div>
-              </div>
-              <div className="restaurant-container__card">
-                  <div className="restaurant_container__card-naming">
-                   <img src="./images/restaurant_3.png" alt="img-error" />
-                          <p>Полли</p>
-                        <div className="restaurant_container__card-discription">
-                          <div className="restaurant_container_card__discription-contact">
-                             <img src="./images/restaurant_stars.png" alt="img-error" />
-                             <p>Молодогвардейцев, 84</p>
-                          </div>
-                          <div className="restaurant_container_card__discription-price">
-                              <img src="./images/price.png" alt="img-error" />
-                              <p>12:00 - 22:00</p>
-                          </div>                                            
-                      </div>
-                  </div>
-              </div>
-              <div className="restaurant-container__card">
-                  <div className="restaurant_container__card-naming">
-                   <img src="./images/restaurant_3.png" alt="img-error" />
-                          <p>Полли</p>
-                        <div className="restaurant_container__card-discription">
-                          <div className="restaurant_container_card__discription-contact">
-                             <img src="./images/restaurant_stars.png" alt="img-error" />
-                             <p>Молодогвардейцев, 84</p>
-                          </div>
-                          <div className="restaurant_container_card__discription-price">
-                              <img src="./images/price.png" alt="img-error" />
-                              <p>12:00 - 22:00</p>
-                          </div>                                            
-                      </div>
-                   </div>
-              </div>
-          </div>
-        </div>
+      <MainCardList data={data}/>
     </div>
   )
 }
