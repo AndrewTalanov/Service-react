bfa7c99b90970d6e23142bdd32059d1f94713011
AndrewBavyka <abavyka@list.ru>
2021-11-27 00:45:04 +0500

    Изменили шапку и добавили поиск

--- src/header/header-search-panel/header-search-panel.js
+++ src/header/header-search-panel/header-search-panel.js
@@ -2,9 +2,14 @@ import './header-search-panel.css';
 
 const HeaderSearchPanel = () => {
   return (
-    <div className="header-form-search-panel">
-      <img src="./images/icon-search.svg" alt="img" />
-    </div>
+    <div className="header-form-search-panel">     
+        <form class="header-search">
+          <input className = "search-panel" type="text" placeholder="Поиск" name="search"></input>
+            <button type="submit" class="button-search" >
+              <img src="./images/icon-search.svg" alt="img"/>
+            </button>    
+        </form>
+    </div>   
   )
 }
 
