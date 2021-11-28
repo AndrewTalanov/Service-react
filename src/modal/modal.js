5b12b9ecc726d4b1427043a13444428cc43b7acc
Andrew Talanov <andrewtalanov@mail.ru>
2021-11-28 16:19:36 +0500

    ..

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -1,7 +1,6 @@
 import './modal.css';
 
 const Modal = ({visibleModal, toggleModal}) => {
-    // onClick={() => toggleModal(false)}
     return(
         <div className= {visibleModal ? "modal active" : "modal"}>
             <div className="modal__content" onClick={ e => e.stopPropagation()}>
