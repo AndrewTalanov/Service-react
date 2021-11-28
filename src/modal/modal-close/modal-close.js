033cb39c1d0265ea630a13b5f281bc7a1e1f1d6d
AndrewBavyka <abavyka@list.ru>
2021-11-28 16:16:46 +0500

    Починили адаптив модального окна и немного фиксов)

--- src/modal/modal-close/modal-close.js
+++ src/modal/modal-close/modal-close.js
@@ -2,7 +2,11 @@ import './modal-close.css';
 
 const ModalClose = () => {
   return (
-    <div></div>
+    <div className = "close-modal-window">
+        <button className = "close-modal-window__btn">
+          <img src="./images/close-modal.svg" alt="" />
+        </button>
+    </div>
   )
 }
 
--- src/modal/modal.js
+++ src/modal/modal.js
@@ -1,11 +1,14 @@
 import './modal.css';
+import ModalClose from './modal-close/modal-close';
 
 const Modal = ({visibleModal, toggleModal}) => {
     // onClick={() => toggleModal(false)}
     return(
         <div className= {visibleModal ? "modal active" : "modal"}>
             <div className="modal__content" onClick={ e => e.stopPropagation()}>
-
+                <ModalClose>
+               
+                </ModalClose>
             </div>
         </div>
     )
